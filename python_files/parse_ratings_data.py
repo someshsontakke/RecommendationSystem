3c933714e26cbffb78b96a7462dfde627fbfab97
Somesh Sontakke <sontakkesomesh@gmail.com>
2015-03-24 16:26:14 +0530

    Created parsers for the data files.

--- python_files/parse_ratings_data.py
+++ python_files/parse_ratings_data.py
@@ -1,34 +1,37 @@
 #python file to parse the ratings.dat file
-data_array = []
-counter = 0
-dubbu = 0
-with open("ratings.dat") as f:
-	for line in f:
-		if dubbu>2:
-			break
-		data_array.append([])
-		colon_counter = 0
-		number = 0
-		num_list = ['1','2','3','4','5','6','7','8','9','0']
-		for i in line:
-			#print i
-			if i == ":":
-				colon_counter += 1
-			if colon_counter%2 == 1:
-				data_array[counter].append(number)
-				print number
-				number = 0
-				continue
-			if i in num_list:
+def parse_ratings_data():
+	data_array = []
+	counter = 0
+	with open("ratings.dat") as f:
+		for line in f:
+			if counter>10000:
+				break
+			data_array.append([])
+			colon_counter = 0
+			number = 0
+			num_list = ['1','2','3','4','5','6','7','8','9','0']
+			for i in line:
 				#print i
-				number = number*10 + int(i)
-				#print number
-			if i =='\n':
-				data_array[counter].append(number)
-				print number
-				number = 0
-		counter +=1
-		dubbu +=1
-print data_array[:20]
+				if i == ":":
+					colon_counter += 1
+				if colon_counter%2 == 1:
+					data_array[counter].append(number)
+					#print number
+					number = 0
+					continue
+				if i in num_list:
+					#print i
+					number = number*10 + int(i)
+					#print number
+				if i =='\n':
+					data_array[counter].append(number)
+					#print number
+					number = 0
+			counter +=1
+	#print data_array[1900:2000]
+	return data_array
+
+a = parse_ratings_data()[:200]
+print a
 
 
---
+++ python_files/parse_users_data.py
@@ -0,0 +1,49 @@
+#python file to parse the users.dat file
+data_array = []
+counter = 0
+working_string = ''
+with open('users.dat') as f:
+	for line in f:
+		if counter > 1:
+			break
+		for i in range(0,len(line)):
+			if line[i]==':' and line[i-1]==':':
+				working_string = working_string + '^'
+			elif line[i]==':':
+				a=1
+			else:
+				#print line[i]
+				working_string = working_string + line[i]
+#print working_string[:200]
+counter = 0
+flag = 0
+number = ''
+for i in working_string:
+	data_array.append([])
+	if i == '^':
+		data_array[counter].append(number)
+		number = ''
+		continue
+	elif i =='\n':
+		data_array[counter].append(number)
+		#print number
+		number = ''
+		flag = 1
+		counter +=1
+	else:
+		number = number + i
+
+temp = data_array[:6040]
+data_array = temp
+
+for i in range(0,len(data_array)):
+	data_array[i][0] = int(data_array[i][0])
+	data_array[i][2] = int(data_array[i][2])
+	data_array[i][3] = int(data_array[i][3])
+	data_array[i][4] = int(data_array[i][4][:5])
+	if data_array[i][1] == 'M':
+		data_array[i][1] = 0
+	else:
+		data_array[i][1] = 1
+print data_array[10]
+
